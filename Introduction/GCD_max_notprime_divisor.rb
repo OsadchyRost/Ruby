5d90d368e3bfb3f2959311e1b5546bc426891062
OsadchyRost <rostisl.osadchy@yandex.ru>
2024-11-08 21:32:12 +0300

    Lab 1 | Task 4 | GCD
    # Метод 3 Найти НОД максимального нечетного непростого делителя числа и произведения цифр данного числа.

--- Introduction/GCD_max_notprime_divisor.rb
+++ Introduction/GCD_max_notprime_divisor.rb
@@ -1,56 +1,48 @@
 # Метод 3 Найти НОД максимального нечетного непростого делителя числа и произведения цифр данного числа.
 
-def nod(a, b)
+def gcd(num)
+
+  def is_prime(n)
+    return false if n <= 1
+  
+    (2..n/2).each do |i|
+      return false if n % i == 0
+    end
+  
+    return true
+  end
+
+  def maxi_div(n)
+    max_res = -1
+    (3..n).step(2).each do |i|  
+      if n % i == 0
+        if !is_prime(i)
+          max_res = [max_res, i].max
+        end
+      end
+    end
+    max_res
+  end
+
+  def product(a)
+    res = 1
+    while a > 0 do
+      res *= a % 10
+      a /= 10
+    end
+    return res
+  end
+  
+  a = maxi_div(num)
+  b = product(num)
   while b != 0
     a, b = b, a % b
   end
   a
 end
 
-def summ(a)
-  res = 1
-  while a > 0 do
-    res *= a % 10
-    a /= 10
-  end
-  return res
-end
-
-def is_prime(n)
-  return false if n <= 1
-
-	(2..n/2).each do |i|
-		return false if n % i == 0
-	end
-
-	return true
-end
-
-def maxi_found(n)
-  max_res = -1
-  (3..n).step(2).each do |i|  
-    if n % i == 0
-      if !is_prime(i)
-        max_res = [max_res, i].max
-      end
-    end
-  end
-  max_res
-end
 
 print "Введите число: "
 number = gets.chomp.to_i
-result = maxi_found(number)
-sum=summ(number)
-
-puts "Произвдеение цифр числа: #{sum}"
-if result == -1
-  puts "Нет нечетных непростых делителей"
-else
-  puts "Ответ: #{result}"
-end
-
-answer = nod(sum, result)
+answer = gcd(number)
 puts "НОД: #{answer}"
-
-
