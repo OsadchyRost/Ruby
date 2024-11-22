67f77f35e7a23869d22a04152cf28ee0def8470d
OsadchyRost <rostisl.osadchy@yandex.ru>
2024-11-22 21:28:53 +0300

    lab 2 - task 1.7
    Валидация номера телефона. Модифицирован конструктор, геттер и сеттер для номера телефона. Изменен main для тестирования изменений

--- Core_classes_and_models/Student.rb
+++ Core_classes_and_models/Student.rb
@@ -1,19 +1,32 @@
 class Student
-  def initialize(surname, first_name, second_name, **params)
+    def initialize(surname, first_name, second_name, **params)
 		@surname = surname
 		@first_name = first_name
 		@second_name = second_name
 		@id = params[:id]
-		@phone = params[:phone]
+		self.phone = params[:phone]
 		@telegram = params[:telegram]
 		@mail = params[:mail]
 		@git = params[:git]
 	end
   
-    attr_accessor :id, :surname, :first_name, :second_name, :phone, :telegram, :mail, :git
+    attr_accessor  :surname, :first_name, :second_name, :id, :telegram, :mail, :git
 
+    attr_reader :phone
+
+    def phone=(val)
+		if self.class.valid_phone_num?(val)
+			@phone = val
+		else 
+			puts "#{surname} #{first_name} #{second_name}: Некорректный номер телефона"
+		end
+	end
+
+    def self.valid_phone_num?(str)
+		str.nil? || str.match?(/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/)
+	end
+    
     def to_s
         "ID: #{id || 'не указан'}, ФИО: #{surname} #{first_name} #{second_name}, Телефон: #{phone || 'не указан'}, Телеграм: #{telegram || 'не указан'}, Почта: #{mail || 'не указана'}, Git: #{git || 'не указан'}"
     end
-    
 end
--- Core_classes_and_models/main.rb
+++ Core_classes_and_models/main.rb
@@ -1,6 +1,6 @@
 require './student.rb'
 
-student1 = Student.new("Иванов", "Иван", "Иванович", id: 1,  phone: "89447142211", telegram: "sdg", mail: "sdf@mail.ru", git: "github.com/irehyvan")
+student1 = Student.new("Иванов", "Иван", "Иванович", id: 1,  phone: "fhzdh", telegram: "sdg", mail: "sdf@mail.ru", git: "github.com/irehyvan")
 student2 = Student.new("Иванова", "Мария", "Ивановна", id: 2,  phone: "89447142211", mail: "sdf@mail.ru", git: "github.com/irehyvan")
 
 puts student1
